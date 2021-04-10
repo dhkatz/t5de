5e59c72ed34a05d5ce8489a577bfe8e855150f8d
dhkatz <dhkatz.16@gmail.com>
2021-04-10 15:16:24 -0700

    Add GitHub actions for building

---
+++ install.nsi
@@ -0,0 +1,133 @@
+!include "FileFunc.nsh"
+!include "MUI2.nsh"
+
+!define APPNAME "T5DE"
+!define IMVU_VERSION $%VERSION%
+
+!define APP_VERSION "${IMVU_VERSION}.0.0"
+!define APP_EXE "IMVUClient.exe"
+
+!define APP_UNINSTALL "Software\Microsoft\Windows\CurrentVersion\Uninstall\${APPNAME}"
+
+; General
+
+  Name "${APPNAME}"
+  BrandingText "T5DE ${IMVU_VERSION}"
+
+  OutFile "${APPNAME}-${IMVU_VERSION}.exe"
+  Unicode true
+
+  InstallDir "$LOCALAPPDATA\${APPNAME}"
+  InstallDirRegKey HKCU "Software\${APPNAME}" ""
+
+  RequestExecutionLevel user
+
+  ShowInstDetails show
+
+; Compiler Flags
+
+  SetCompressor lzma
+  SetDateSave off
+
+; Variables
+
+  Var SMDIR
+
+; Interface Settings
+  !define MUI_ICON "t5de.ico"
+  !define MUI_UNICON "t5de.ico"
+
+  !define MUI_ABORTWARNING
+  !define MUI_FINISHPAGE_NOAUTOCLOSE
+  !define MUI_UNFINISHPAGE_NOAUTOCLOSE
+
+  !define MUI_FINISHPAGE_SHOWREADME ""
+  !define MUI_FINISHPAGE_SHOWREADME_TEXT "Create Desktop Shortcut"
+  !define MUI_FINISHPAGE_SHOWREADME_FUNCTION CreateDesktopShortcut
+
+  !define MUI_FINISHPAGE_RUN "$INSTDIR\${APP_EXE}"
+
+; Pages
+
+  !insertmacro MUI_PAGE_WELCOME
+  !insertmacro MUI_PAGE_DIRECTORY
+
+  ;Start Menu Folder Page Configuration
+  !define MUI_STARTMENUPAGE_REGISTRY_ROOT "HKCU" 
+  !define MUI_STARTMENUPAGE_REGISTRY_KEY "Software\${APPNAME}" 
+  !define MUI_STARTMENUPAGE_REGISTRY_VALUENAME "Start Menu Folder"
+  !define MUI_STARTMENUPAGE_DEFAULTFOLDER "${APPNAME}"
+
+  !insertmacro MUI_PAGE_STARTMENU Application $SMDIR
+
+  !insertmacro MUI_PAGE_INSTFILES
+  !insertmacro MUI_PAGE_FINISH
+
+  !insertmacro MUI_UNPAGE_WELCOME
+  !insertmacro MUI_UNPAGE_CONFIRM
+  !insertmacro MUI_UNPAGE_INSTFILES
+  !insertmacro MUI_UNPAGE_FINISH
+
+;Languages
+ 
+  !insertmacro MUI_LANGUAGE "English"
+
+; Versioning
+
+  VIProductVersion "${APP_VERSION}"
+  VIFileVersion "${APP_VERSION}"
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "FileVersion" "${APP_VERSION}"
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "LegalCopyright" "${APPNAME}"
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "ProductName" "${APPNAME}"
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "ProductVersion" "${APP_VERSION}"
+  VIAddVersionKey /LANG=${LANG_ENGLISH} "FileDescription" "Application Installer"
+
+; Sections
+
+Section "${APPNAME}" SecInstall
+  SectionIn RO
+
+  SetOutPath "$INSTDIR"
+
+  File /nonfatal /r ".\IMVUClient\*"
+
+  WriteUninstaller "$INSTDIR\${APPNAME} Uninstall.exe"
+
+  ${GetSize} "$INSTDIR" "" $0 $1 $2
+
+  WriteRegStr HKCU "Software\${APPNAME}" "" $INSTDIR
+
+  WriteRegStr HKLM "${APP_UNINSTALL}" "DisplayName" "${APPNAME}"
+  WriteRegStr HKLM "${APP_UNINSTALL}" "DisplayVersion" "${APP_VERSION}"
+  WriteRegStr HKLM "${APP_UNINSTALL}" "DisplayIcon" "$INSTDIR\${APPNAME} Uninstall.exe"
+  WriteRegStr HKLM "${APP_UNINSTALL}" "UninstallString" "$INSTDIR\${APPNAME} Uninstall.exe"
+  WriteRegStr HKLM "${APP_UNINSTALL}" "InstallLocation" "$INSTDIR"
+  WriteRegDWORD HKLM "${APP_UNINSTALL}" "EstimatedSize" "$0"
+
+  !insertmacro MUI_STARTMENU_WRITE_BEGIN Application
+
+    CreateDirectory "$SMPROGRAMS\$SMDIR"
+    CreateShortCut "$SMPROGRAMS\$SMDIR\${APPNAME}.lnk" "$INSTDIR\${APP_EXE}" "" "$INSTDIR\${APPNAME} Uninstall.exe"
+    CreateShortCut "$SMPROGRAMS\$SMDIR\Uninstall ${APPNAME}.lnk" "$INSTDIR\${APPNAME} Uninstall.exe" "" "$INSTDIR\${APPNAME} Uninstall.exe"
+  
+  !insertmacro MUI_STARTMENU_WRITE_END
+SectionEnd
+
+Section "Uninstall"
+  RMDir /r /REBOOTOK $INSTDIR
+
+  !insertmacro MUI_STARTMENU_GETFOLDER Application $SMDIR
+
+  Delete "$SMPROGRAMS\$SMDIR\${APPNAME}.lnk"
+  Delete "$SMPROGRAMS\$SMDIR\Uninstall ${APPNAME}.lnk"
+  Delete "$DESKTOP\${APPNAME}.lnk"
+  
+  RMDIR "$SMPROGRAMS\$SMDIR"
+
+  DeleteRegKey HKCU "Software\${APPNAME}"
+  DeleteRegKey HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\${APPNAME}"
+SectionEnd
+
+Function CreateDesktopShortcut
+  CreateShortCut "$DESKTOP\${APPNAME}.lnk" "$INSTDIR\${APP_EXE}" "" "$INSTDIR\${APPNAME} Uninstall.exe"
+FunctionEnd
--- main.py
+++ main.py
@@ -1,33 +1,87 @@
 # coding=utf-8
 
 import os
+import shutil
 import sys
 import re
+import zipfile
+import requests
+import autogui
+import time
 
 from uncompyle6 import decompile_file
 from py_compile import compile
 
 from patch import ClientAppPatch, AccountPatch, ProductLoaderPatch, WindowsPatch, SessionWindowPatch
-from install import download, install, extract
 
-path = os.path.dirname(sys.argv[0])
-patches = [AccountPatch(), ClientAppPatch(), ProductLoaderPatch(), WindowsPatch(), SessionWindowPatch()]
+PATH = os.path.dirname(sys.argv[0])
+BASE_URL = 'https://static-akm.imvu.com/imvufiles/installers/InstallIMVU_{}.exe'
+CLIENT_PATH = '{}/IMVUClient'.format(os.getenv('APPDATA'))
+VERSION = '539.14'
+PATCHES = [AccountPatch(), ClientAppPatch(), ProductLoaderPatch(), WindowsPatch(), SessionWindowPatch()]
+
+
+def download():
+    print('DOWNLOADING: IMVU {}'.format(VERSION))
+    if os.path.isfile('{}/InstallIMVU_{}.exe'.format(PATH, VERSION)):
+        return
+
+    r = requests.get(BASE_URL.format(VERSION))
+
+    with open('{}/{}'.format(PATH, 'InstallIMVU_{}.exe'.format(VERSION)), 'wb') as f:
+        f.write(r.content)
+
+
+def install():
+    print('INSTALLING: IMVU {}'.format(VERSION))
+    autogui.open('InstallIMVU_539.14.exe', setActive=False)
+    autogui.setWindow('IMVU Setup')
+    autogui.click('Install', 0, 4)
+    print('SLEEPING: 15 SECONDS...')
+    time.sleep(15)
+
+    attempts = 0
+    while attempts < 5:
+        attempts += 1
+        try:
+            autogui.setWindow('IMVU Login')
+            autogui.click('Close', 0, 4)
+            break
+        except Exception:
+            if attempts > 5:
+                raise RuntimeError('Could not automate IMVU install! Failed to find Login window.')
+            print('SLEEPING: 5 SECONDS...')
+            time.sleep(5)
+
+    print('INSTALLED: IMVU {}'.format(VERSION))
+
+
+def copy():
+    print('COPYING: {}'.format(CLIENT_PATH))
+    if not os.path.isdir(os.path.join(PATH, 'IMVUClient')):
+        shutil.copytree(CLIENT_PATH, os.path.join(PATH, 'IMVUClient'))
+
+
+def extract():
+    print('EXTRACTING: LIBRARY.ZIP')
+    with zipfile.ZipFile('{}/IMVUClient/library.zip'.format(PATH), 'r') as zip_file:
+        zip_file.extractall('{}/library'.format(PATH))
 
 
 def decompile():
-    for p in patches:
+    for p in PATCHES:
         print('DECOMPILING: {}'.format(p.file))
-        with open("{}/{}.py".format(path, p.file), "w") as f:
-            decompile_file("{}/{}.pyo".format(path, p.file), f)
+        with open("{}/library/{}.py".format(PATH, p.file), "w") as f:
+            decompile_file("{}/library/{}.pyo".format(PATH, p.file), f)
 
 
 def patch():
-    for p in patches:
+    for p in PATCHES:
         output = []
 
         print('PATCHING: {}'.format(p.file))
 
-        with open("{}/{}.py".format(path, p.file)) as f:
+        with open("{}/library/{}.py".format(PATH, p.file)) as f:
             source = f.readlines()
 
             i = 0
@@ -44,32 +98,53 @@ def patch():
                 if not found:
                     output.append(line)
 
-        with open("{}/{}.py".format(path, p.file), "w") as f:
+        with open("{}/library/{}.py".format(PATH, p.file), "w") as f:
             f.writelines(output)
 
 
 def recompile():
-    for p in patches:
+    for p in PATCHES:
         print('COMPILING: {}'.format(p.file))
-        compile('{}/{}.py'.format(path, p.file), '{}/{}.pyo'.format(path, p.file), '-o', doraise=True)
+        compile('{}/library/{}.py'.format(PATH, p.file), '{}/library/{}.pyo'.format(PATH, p.file), '-o', doraise=True)
 
 
 def cleanup():
-    for p in patches:
+    for p in PATCHES:
         print('CLEANING: {}'.format(p.file))
-        filepath = '{}/{}.py'.format(path, p.file)
+        filepath = '{}/library/{}.py'.format(PATH, p.file)
         if os.path.isfile(filepath):
             os.remove(filepath)
 
 
-# Press the green button in the gutter to run the script.
+def archive():
+    print('ARCHIVING: LIBRARY.ZIP')
+
+    lib_dir = os.path.join(PATH, 'library')
+
+    with zipfile.ZipFile(os.path.join(PATH, 'library.zip'), 'w', compression=zipfile.ZIP_STORED) as zip_file:
+        for root, dirs, files in os.walk(lib_dir):
+            for filename in files:
+                filepath = os.path.join(root, filename)
+                zip_file.write(filepath, os.path.relpath(filepath, lib_dir))
+
+    print('ARCHIVED: LIBRARY.ZIP')
+
+
+def replace():
+    print('REPLACING: LIBRARY.ZIP')
+
+    os.remove(os.path.join(PATH, 'IMVUClient', 'library.zip'))
+    shutil.move(os.path.join(PATH, 'library.zip'), os.path.join(PATH, 'IMVUClient'))
+
+
 if __name__ == '__main__':
     download()
     install()
+    copy()
     extract()
     decompile()
     patch()
     recompile()
     cleanup()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    archive()
+    replace()
