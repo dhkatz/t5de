2299e387b0903f2744582876ac0b46fc5172e801
dhkatz <dhkatz.16@gmail.com>
2024-05-17 23:28:10 -0700

    fix[patcher]: Re-enable the chat, *hiresnobg, and *hiressnap in shop mode

--- t5de/patches/python/EnablePhotoModePatch.py
+++ t5de/patches/python/EnablePhotoModePatch.py
@@ -6,6 +6,9 @@ class EnablePhotoModePatch(PythonPatch):
         super(EnablePhotoModePatch, self).__init__()
 
         self.register('ENABLE_PHOTO_MODE', 'imvu/mode/ShopMode.py', 'def addChatTool')
+        self.register('ENABLE_CHAT_TOOL', 'imvu/mode/ShopMode.py', 'def addChatTool')
+        self.register('ENABLE_IN_SHOP_1', 'imvu/client/sessionwindow.py', 'def __hiResSnapshot')
+        self.register('ENABLE_IN_SHOP_2', 'imvu/client/sessionwindow.py', 'def __hiResSnapshotNoBg')
 
     def patch(self, context):
         if context.pattern == 'ENABLE_PHOTO_MODE':
@@ -14,3 +17,15 @@ class EnablePhotoModePatch(PythonPatch):
             context.write('return [\'invite-friend\']\n', indent=2)
 
             context.write(context.line)
+        elif context.pattern == 'ENABLE_CHAT_TOOL':
+            context.write(context.line)
+            context.skip(1)
+            context.write('self.addChatToolNoCheck()', indent=2)
+
+            context.write(context.line)
+        elif context.pattern == 'ENABLE_IN_SHOP_1':
+            context.write(context.line)
+            context.skip(2)
+        elif context.pattern == 'ENABLE_IN_SHOP_2':
+            context.write(context.line)
+            context.skip(2)
